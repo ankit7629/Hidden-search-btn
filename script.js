394c9e262e8e0c1354c80e4f60e3330472e467af
unknown <ankitthakur7629@gmail.com>
2023-07-16 00:07:58 +0530

    files are added

---
+++ script.js
@@ -0,0 +1,7 @@
+const search = document.querySelector('.search');
+const inputBtn = document.querySelector('.input');
+const searchBtn = document.querySelector('.btn');
+
+searchBtn.addEventListener('click', ()=>{
+    search.classList.toggle('active')
+})
